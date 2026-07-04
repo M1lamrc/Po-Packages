fa213499208ad3a5e871aa1a4c23a8659167bad4
ML14 <ismailefedee@gmail.com>
2026-07-04 16:26:52 +0300

    Update logo.cmd

--- logo.cmd
+++ logo.cmd
@@ -1,18 +1,21 @@
 @echo off
 color 0B
 echo.
-echo  PPPPPPPPP 
-echo  P::::::::P
-echo  P::::::::P
-echo  PP::::::PP
-echo    P:::::P 
-echo    P:::::P 
-echo    P:::::P 
-echo  PP:::::::PP
-echo  P:::::::::P
-echo  P:::::::::P
-echo  PPPPPPPPPPP
+echo  XXXXXXXX           XXXXXXXX
+echo   XXXXXXXX         XXXXXXXX
+echo    XXXXXXXX       XXXXXXXX
+echo     XXXXXXXX     XXXXXXXX
+echo      XXXXXXXX   XXXXXXXX
+echo       XXXXXXXXXXXXXXXX
+echo        XXXXXXXXXXXXXX
+echo        XXXXXXXXXXXXXX
+echo       XXXXXXXXXXXXXXXX
+echo      XXXXXXXX   XXXXXXXX
+echo     XXXXXXXX     XXXXXXXX
+echo    XXXXXXXX       XXXXXXXX
+echo   XXXXXXXX         XXXXXXXX
+echo  XXXXXXXX           XXXXXXXX
 echo.
-echo  Po Framework v0.1
+echo    Po Framework v0.1
 echo.
 color 07
