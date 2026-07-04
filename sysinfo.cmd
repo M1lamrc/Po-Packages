71b8409b059ffef7f2966a3af1224ea8b8a74a5f
ML14 <ismailefedee@gmail.com>
2026-07-04 15:41:18 +0300

    Update sysinfo.cmd

---
+++ sysinfo.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo ========================
+echo Po System Info [v1.0]
+echo ========================
+echo Framework: Po v0.1
+echo Status: System Online
+echo ========================
